5cc75af9f291324974aeb875ca8e63230a7e3825
arvind <arvindyuvasoft112@gmail.com>
2014-07-16 19:30:31 +0530

    changes for socket

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :repo_url, 'git@github.com:sandy1987/peatio.git'
 
 
 # Default value for default_env is {}
- set :default_env, { path: "/opt/ruby/bin:$PATH" }
+ #set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
  set :keep_releases, 5
